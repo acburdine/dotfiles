c9bd1072917cb5277d0bd16f767b81170649921c
Austin Burdine <austin@burdine.me>
2025-08-12 14:35:56 -0400

    update mason/brew apps

--- nvim/config/lua/acburdine/lazy/lsp.lua
+++ nvim/config/lua/acburdine/lazy/lsp.lua
@@ -19,20 +19,28 @@ return {
     local cmp_lsp = require("cmp_nvim_lsp")
 
     local capabilities =
-        vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(), cmp_lsp.default_capabilities())
+      vim.tbl_deep_extend("force", {}, vim.lsp.protocol.make_client_capabilities(), cmp_lsp.default_capabilities())
 
     require("fidget").setup({})
     require("mason").setup()
     require("mason-lspconfig").setup({
       ensure_installed = {
-        "lua_ls",
-        -- "rust_analyzer",
+        "dockerls",
+        "efm",
+        "eslint",
         "gopls",
         "graphql",
         "html",
-        "dockerls",
+        "jsonls",
+        "lua_ls",
+        "prismals",
+        "pyright",
+        "pylsp",
+        -- "rust_analyzer",
         "tailwindcss",
+        "taplo",
         "terraformls",
+        "ts_ls",
         "yamlls",
       },
 
