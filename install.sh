70d3c5e51d1056f469cca4c3a3842dda30488ce0
Austin Burdine <austin@burdine.me>
2025-08-14 23:18:01 -0400

    cleanup: remove old bash_profile, add basic install script (#9)

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# first, make sure we're in the home dir
+cd ~
+
+# second, clone the repo
+git clone https://github.com/acburdine/dotfiles dotfiles
+
+# third, cd into dotfiles repo
+cd ~/dotfiles
+
+# lastly, start setup script
+bash ./setup.sh "$1"
