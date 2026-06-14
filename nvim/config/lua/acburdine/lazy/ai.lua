8e5674f0c43540f77a53aa1a53b7a1f5b7a26b74
Austin Burdine <austin@burdine.me>
2026-06-14 10:12:54 -0400

    updates

--- nvim/config/lua/acburdine/lazy/ai.lua
+++ nvim/config/lua/acburdine/lazy/ai.lua
@@ -27,19 +27,98 @@ return {
   --     end)
   --   end,
   -- },
+  -- {
+  --   "olimorris/codecompanion.nvim",
+  --   opts = {},
+  --   dependencies = {
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-treesitter/nvim-treesitter",
+  --   },
+
+  --   config = function()
+  --     require("codecompanion").setup({
+  --       strategies = {
+  --         chat = {
+  --           adapter = "copilot",
+  --         },
+  --       },
+  --     })
+  --   end,
+  -- },
   {
-    "olimorris/codecompanion.nvim",
-    opts = {},
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    version = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "nvim-treesitter/nvim-treesitter",
+      "MunifTanjim/nui.nvim",
+      "nvim-telescope/telescope.nvim",
+      "hrsh7th/nvim-cmp",
+      "ibhagwan/fzf-lua",
+      "nvim-tree/nvim-web-devicons",
+      "zbirenbaum/copilot.lua",
     },
-
     config = function()
-      require("codecompanion").setup({
-        strategies = {
-          chat = {
-            adapter = "copilot",
+      require("avante").setup({
+        provider = "copilot",
+        behavior = {
+          auto_suggestions = false,
+        },
+        system_prompt = function()
+          local hub = require("mcphub").get_hub_instance()
+          return hub and hub:get_active_servers_prompt() or ""
+        end,
+        custom_tools = function()
+          return {
+            require("mcphub.extensions.avante").mcp_tool(),
+          }
+        end,
+        -- disable these in favor of ones from mcphub
+        disabled_tools = {
+          "list_files",
+          "search_files",
+          "read_file",
+          "create_file",
+          "rename_file",
+          "delete_file",
+          "create_dir",
+          "rename_dir",
+          "delete_dir",
+          "bash",
+        },
+      })
+    end,
+  },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    opts = {
+      file_types = { "markdown", "Avante" },
+    },
+    ft = { "markdown", "Avante" },
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+  },
+  {
+    "ravitemer/mcphub.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    build = "npm install -g mcp-hub@latest", -- Installs `mcp-hub` node binary globally
+    config = function()
+      require("mcphub").setup({
+        config = vim.fn.expand("~/.config/mcphub/servers.json"),
+        workspace = {
+          enabled = true,
+          look_for = { ".mcphub/servers.json", ".vscode/mcp.json", ".cursor/mcp.json" },
+          reload_on_dir_changed = true,
+          port_range = { min = 40000, max = 41000 },
+          get_port = nil,
+        },
+
+        auto_approve = false,
+        auto_toggle_mcp_servers = true,
+        extensions = {
+          avante = {
+            make_slash_commands = true,
           },
         },
       })
